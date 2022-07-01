d41a518fc3cd4884e7312899f234ed25402bd423
resul_dagdanov <resul.dagdanov@eatron.com>
2022-07-01 22:54:03 +0300

    register custom gym environment

---
+++ highway_environment/create_env.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import gym
+
+from gym.envs.registration import register
+
+
+if __name__ == "__main__":
+
+    # one previous directory reach
+    parent_directory = os.path.abspath('..')
+
+    # add parent directory to path
+    sys.path.append(parent_directory)
+
+    register(
+        id='highway-environment-v0',
+        entry_point='highway_environment.src.environment:Environment',
+    )
+
+    env = gym.make('highway-environment-v0')
+
+    print("Environment is successfully created as : ", env)
