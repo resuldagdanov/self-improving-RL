d1e6347036f0233064b2076a9ed2815acd2500eb
resul_dagdanov <resul.dagdanov@eatron.com>
2022-07-10 22:00:25 +0300

    complete main verification agent class

--- experiments/agents/main_agent.py
+++ experiments/agents/main_agent.py
@@ -1,31 +1,31 @@
 import os
+import sys
 import yaml
 import numpy as np
 
 from ray import tune
+from ray.tune.logger import pretty_print
 from ray.rllib.agents.ppo import PPOTrainer
 
+sys.path.append(os.path.join(os.environ["BLACK_BOX"]))
+
 from highway_environment.envs import Environment
+from experiments.utils import validation_utils
 
 
 class MainAgent:
 
     def __init__(self, algorithm_config: dict) -> None:
-        self.repo_path = os.path.join(os.environ["BLACK_BOX"], "experiments")
-
-        self.max_eps_length = algorithm_config["max_eps_length"]
-        self.experiment_name = algorithm_config["experiment_name"]
-
-    def __str__(self) -> str:
-        return "Agent Folder: %s\n", "Checkpoint Number: %d"
+        self.repo_path = validation_utils.repo_path
+        self.algorithm_config = algorithm_config
     
     @staticmethod
     def create_search_space(params: dict) -> tuple:
         distance_space = list(
             np.linspace(
-                start   =   params['distance']['min'],
-                stop    =   params['distance']['max'],
-                num     =   params['distance']['steps'],
+                start   =   params["distance"]["min"],
+                stop    =   params["distance"]["max"],
+                num     =   params["distance"]["steps"],
                 endpoint=   True,
                 retstep =   True,
                 dtype   =   np.float32
@@ -33,9 +33,9 @@ class MainAgent:
         )
         velocity_space = list(
             np.linspace(
-                start   =   params['velocity']['min'],
-                stop    =   params['velocity']['max'],
-                num     =   params['velocity']['steps'],
+                start   =   params["velocity"]["min"],
+                stop    =   params["velocity"]["max"],
+                num     =   params["velocity"]["steps"],
                 endpoint=   True,
                 retstep =   True,
                 dtype   =   np.float32
@@ -44,29 +44,128 @@ class MainAgent:
 
         return distance_space, velocity_space
     
-    def initialize_config(self, env_config_path: str, algo_config_path: str) -> dict:
+    def initialize_config(self, env_config_path: str, search_config: dict, model_config_path: str) -> dict:
         # highway environment configirations
-        with open(env_config_path) as f:
+        with open(validation_utils.configs_path + env_config_path) as f:
             env_configs = yaml.safe_load(f)
+        
+        # change vehicle initialization parameters according to search space configuration
+        env_configs["set_manually"]["ego_position"] = [ 0.0, 0.0 ]
+        env_configs["set_manually"]["ego_heading"] = 0.0
+        env_configs["set_manually"]["ego_speed"] = search_config["ego_v1"]
+        env_configs["set_manually"]["ego_target_speed"] = 40.0
+
+        env_configs["set_manually"]["front_position"] = [ search_config["delta_dist"], 0.0 ]
+        env_configs["set_manually"]["front_heading"] = 0.0
+        env_configs["set_manually"]["front_speed"] = search_config["front_v1"]
+        env_configs["set_manually"]["front_target_speed"] = search_config["front_v2"]
 
         # training algorithms configurations
-        with open(algo_config_path) as f:
-            algo_configs = yaml.safe_load(f)
+        with open(validation_utils.configs_path + model_config_path) as f:
+            model_configs = yaml.safe_load(f)
         
         # add environment configurations to training config
         general_config = model_configs.copy()
-        general_config["train"]["config"]["env_config"] = env_configs
+        general_config["env_config"] = env_configs
 
+        print("\n[CONFIG]-> General Configurations:\t", pretty_print(general_config))
         return general_config
 
-    def initialize_model(self, params: dict):
-        pass
+    def initialize_model(self, general_config: dict) -> object:
+        trainer = PPOTrainer(config=general_config, env=general_config["env"]) # NOTE: change this line when model different than PPO is used
+        print("\n[INFO]-> Trainer:\t", trainer)
 
-    def initialize_environment(self, config: dict):
-        pass
+        agent_path = os.path.join(self.repo_path, "results/trained_models/" + self.algorithm_config["load_agent_name"])
+        print("\n[INFO]-> Agent Path:\t", agent_path)
 
-    def run_episode(self, env: Environment, agent: PPOTrainer, config: dict):
-        pass
+        checkpoint_num = self.algorithm_config["checkpoint_number"]
+        checkpoint_path = agent_path + "/checkpoint_%06i"%(checkpoint_num) + "/checkpoint-" + str(checkpoint_num)
+        trainer.restore(checkpoint_path)
 
-    def simulate(self, params: dict) -> None:
-        pass
+        print("\n[INFO]-> Restore Checkpoint:\t", checkpoint_path)
+        return trainer
+
+    def initialize_environment(self, env_configs: dict) -> Environment:
+        env = Environment(config=env_configs["config"])
+        
+        print("\n[INFO]-> Environment:\t", env)
+        return
+
+    def run_episode(self, env: Environment, agent: object) -> tuple:        
+        statistics = {
+            "ego_speeds"     :  [],
+            "ego_accels"     :  [],
+            "ego_jerks"      :  [],
+            "ego_actions"    :  [],
+            "ego_rewards"    :  [],
+            "front_positions":  [],
+            "front_speeds"   :  []
+        }
+        is_crashed = False
+        episode_reward = 0.0
+        
+        # get initial observation
+        obs = env.reset()
+
+        # loop until episode is finished or terminated
+        for step_idx in range(self.algorithm_config["max_eps_length"]):
+
+            # get model action prediction
+            action_prediction, state, _ = agent.get_policy().compute_actions([obs]) # NOTE: change this line when model different than PPO is used
+
+            # step in the environment with predicted action to get next state
+            obs, reward, done, info = env.step(action_prediction)
+            episode_reward += reward
+
+            # store information at every step
+            statistics["ego_speeds"].append(info["ego_speed"])
+            statistics["ego_accels"].append(info["ego_accel"])
+            statistics["ego_jerks"].append(info["ego_jerk"])
+            statistics["ego_actions"].append(info["ego_action"])
+            statistics["ego_rewards"].append(reward)
+            statistics["front_positions"].append(info["mio_position"])
+            statistics["front_speeds"].append(info["mio_speed"])
+            
+            is_crashed = info["crashed"]
+            is_terminated = info["terminated"]
+
+            if done:
+                if is_crashed:
+                    print("\n[INFO]-> Vehicle is Crashed!")
+                else:
+                    print("\n[INFO]-> Episode is Finished! Length of Episode:\t", step_idx, "steps")
+                break
+        
+        return step_idx, is_crashed, episode_reward, statistics
+
+    def simulate(self, search_config: dict) -> None:
+        # recall trained model configurations within environment parameters
+        general_config = self.initialize_config(
+            env_config_path     =   "/env_config.yaml",
+            search_config       =   search_config,
+            model_config_path   =   "/ppo_config.yaml", # NOTE: change this line when model different than PPO is used
+        )
+
+        # create environment object with default parameters
+        env = self.initialize_environment(
+            env_configs         =   general_config["env_config"]
+        )
+
+        # load trained rl model checkpoint
+        model = self.initialize_model(
+            general_config      =   general_config
+        )
+
+        # run one simulation and obtain returning parameters
+        episode_steps, is_crashed, total_episode_reward, statistics = self.run_episode(
+                    env         =   env,
+                    agent       =   model
+        )
+
+        # report results to optimize a minimization or a maximization metric variable
+        tune.report(
+                crashed         =   is_crashed,
+                episode_lenght  =   episode_steps,
+                episode_reward  =   total_episode_reward,
+                statistics      =   statistics
+        )
