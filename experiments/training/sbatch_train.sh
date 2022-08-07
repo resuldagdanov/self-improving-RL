fd3455c3d0474b1409fd6aa5d71063a77aea5884
resul_dagdanov <resul.dagdanov@eatron.com>
2022-08-07 13:55:33 +0300

    add sbatch training script

---
+++ experiments/training/sbatch_train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+  
+#SBATCH --nodes=1                   # do not change
+#SBATCH --gres=gpu:0                # id of gpu to use
+#SBATCH --ntasks-per-node=16        # num of cores to allocate
+#SBATCH --time=01-01:01             # Time limit (D-HH:MM)
+#SBATCH -o %j.out                   # Standard output log file
+#SBATCH -e %j.err                   # Standard err log file
+
+which python
+conda list | grep torch
+conda list | grep cuda
+
+python ppo_train.py
