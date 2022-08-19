fde1be2b3c69e9d64d9a930d0ce5a5a093849ae3
resul_dagdanov <resul.dagdanov@eatron.com>
2022-08-19 10:07:22 +0300

    add max episode length to adaptive seq_mc method

--- experiments/algorithms/adaptive_seq_mc_search.py
+++ experiments/algorithms/adaptive_seq_mc_search.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
 
     # get arguments
     args = validation_utils.argument_parser()
-    args.algo_config_file = "adaptive_seq_mc.yaml"
+    args.algo_config_file = "adaptive_seq_mc_search.yaml"
 
     # config for specified algorithm
     algorithm_config = validation_utils.get_algorithm_config(args=args)
