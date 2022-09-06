1a63bbeea715840bd321c82d2a37027807af9035
resul_dagdanov <resul.dagdanov@eatron.com>
2022-09-06 13:01:32 +0300

    metric changed to -> minimize TTC in episode

--- experiments/algorithms/slurm_verification.sh
+++ experiments/algorithms/slurm_verification.sh
@@ -2,7 +2,7 @@
   
 #SBATCH --nodes=1                   # do not change
 #SBATCH --gres=gpu:0                # id of gpu to use
-#SBATCH --ntasks-per-node=16        # num of cores to allocate
+#SBATCH --ntasks-per-node=8         # num of cores to allocate
 #SBATCH --time=01-01:01             # Time limit (D-HH:MM)
 #SBATCH -o %j.out                   # Standard output log file
 #SBATCH -e %j.err                   # Standard err log file
@@ -11,4 +11,9 @@ which python
 conda list | grep torch
 conda list | grep cuda
 
-python ce_search.py
+# uncomment one the following lines to run the corresponding script
+python grid_search.py
+# python monte_carlo_seach.py
+# python bayesian_search.py
+# python ce_search.py
+# python adaptive_seq_mc_search.py
--- experiments/utils/reward_tuning_slurm.sh
+++ experiments/utils/reward_tuning_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
   
 #SBATCH --nodes=1                   # do not change
-#SBATCH --gres=gpu:1                # id of gpu to use
-#SBATCH --ntasks-per-node=30        # num of cores to allocate
+#SBATCH --gres=gpu:0                # id of gpu to use
+#SBATCH --ntasks-per-node=12        # num of cores to allocate
 #SBATCH --time=01-01:01             # Time limit (D-HH:MM)
 #SBATCH -o %j.out                   # Standard output log file
 #SBATCH -e %j.err                   # Standard err log file
