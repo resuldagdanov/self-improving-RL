c7bece5bb811212512a96ebc8dce2bc988fd9c80
resul_dagdanov <resul.dagdanov@eatron.com>
2022-09-08 11:53:26 +0300

    correct cpu usage and worker distributions

--- experiments/algorithms/slurm_verification.sh
+++ experiments/algorithms/slurm_verification.sh
@@ -2,7 +2,7 @@
   
 #SBATCH --nodes=1                   # do not change
 #SBATCH --gres=gpu:0                # id of gpu to use
-#SBATCH --ntasks-per-node=8         # num of cores to allocate
+#SBATCH --ntasks-per-node=10        # num of cores to allocate (cpu * (1/gpu) in configs)
 #SBATCH --time=01-01:01             # Time limit (D-HH:MM)
 #SBATCH -o %j.out                   # Standard output log file
 #SBATCH -e %j.err                   # Standard err log file
