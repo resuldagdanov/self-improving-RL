37afda3d94167662a30a542787c63556f455ebd5
resul_dagdanov <resul.dagdanov@eatron.com>
2022-09-10 17:15:01 +0300

    evaluate trained model on custom scenarios

--- experiments/evaluation/evaluate_model.py
+++ experiments/evaluation/evaluate_model.py
@@ -27,63 +27,108 @@ if __name__ == "__main__":
     elif agent_model_name == "PPO": model_config_name = "/ppo_config.yaml"
     else: raise NotImplementedError("[ERROR]-> only SAC and PPO are implemented so far!")
 
-    # initialize main agent class
-    agent = MainAgent(
-        algorithm_config=eval_config
-    )
-    print("\n[INFO]-> Agent Class:\t", agent)
-
-    # randomly sample evaluation scenarios
-    if eval_config["evaluate_randomly"] is True:
-        initial_conditions = {}
-    else:
-        # set-up initial configurations with given parameters
-        initial_conditions = {
-            "ego_v1"    : eval_config["evaluation_state"]["ego_velocity"]["initial_value"],
-            "front_v1"  : eval_config["evaluation_state"]["front_velocity"]["initial_value"],
-            "front_v2"  : eval_config["evaluation_state"]["front_velocity"]["target_value"],
-            "delta_dist": eval_config["evaluation_state"]["distance"]
-        }
-        print("\n[INFO]-> Initial Conditions:\t", pretty_print(initial_conditions))
-
-    # set project directory for all ray workers
-    runtime_env = {
-        "working_dir": parent_directory,
-        "excludes": ["*.err", "*.out"] # exclude error and output files (relative path from "parent_directory")
-    }
-    ray.init(runtime_env=runtime_env)
-
     # create a folder to store the simulation results at each episode separately
-    eval_folder_name = eval_config["experiment_name"] + "_" + eval_config["load_agent_name"]
+    if eval_config["validation_container"] is None:
+        eval_folder_name = eval_config["experiment_name"] + "_Random_Container_" + eval_config["load_agent_name"]
+    else:
+        eval_folder_name = eval_config["experiment_name"] + "_" + eval_config["validation_container"] + "_Container_" + eval_config["load_agent_name"]
     results_dir = os.path.join(validation_utils.repo_path, "results/evaluation_statistics")
 
     stats_path = os.path.join(results_dir, eval_folder_name)
     if os.path.exists(stats_path) is False:
         os.makedirs(stats_path)
-
-    # recall trained model configurations within environment parameters
-    general_config = agent.initialize_config(
+    
+    # initialize main agent class
+    agent = MainAgent(
+        algorithm_config    =   eval_config
+    )
+    print("\n[INFO]-> Agent Class:\t", agent)
+    
+    if eval_config["validation_container"] is not None:
+        # get verification results dataframe
+        container_df = validation_utils.extract_results_csv(
+            file_path       =   os.path.join(os.path.join(parent_directory,
+                                "experiments/results/validation_checkpoints"),
+                                eval_config["validation_container"], "results.csv")
+        )
+        # sort and select scenarios with minimum time-to-collision metric
+        container_df_sorted = container_df.sort_values(
+            by              =   eval_config["data_sort_metric"],
+            ascending       =   True
+        )
+        container_df_sorted = container_df_sorted[container_df_sorted["impossible"] == False] 
+    else:
+        # randomly sample evaluation scenarios
+        if eval_config["evaluate_randomly"] is True:
+            initial_conditions = {}
+        else:
+            # set-up initial configurations with given parameters
+            initial_conditions = {
+                "ego_v1"    :   eval_config["evaluation_state"]["ego_velocity"]["initial_value"],
+                "front_v1"  :   eval_config["evaluation_state"]["front_velocity"]["initial_value"],
+                "front_v2"  :   eval_config["evaluation_state"]["front_velocity"]["target_value"],
+                "delta_dist":   eval_config["evaluation_state"]["distance"]
+            }
+            print("\n[INFO]-> Initial Conditions:\t", pretty_print(initial_conditions))
+        
+        # recall trained model configurations within environment parameters
+        general_config = agent.initialize_config(
         env_config_path     =   "/env_config.yaml",
         search_config       =   initial_conditions,
         model_config_path   =   model_config_name
-    )
+        )
 
-    # create environment object with default parameters
-    env = agent.initialize_environment(
-        env_configs         =   general_config["env_config"]
-    )
+        # create environment object with default parameters
+        env = agent.initialize_environment(
+            env_configs     =   general_config["env_config"]
+        )
+        # set seed to numpy array of environment randomness
+        env.seed(eval_config["seed"])
+
+        # load trained rl model checkpoint
+        model = agent.initialize_model(
+            general_config  =   general_config,
+            log_folder_path =   stats_path
+        )
     
-    # load trained rl model checkpoint
-    model = agent.initialize_model(
-        general_config      =   general_config,
-        log_folder_path     =   stats_path
-    )
+    # set project directory for all ray workers
+    runtime_env = {
+        "working_dir"       :   parent_directory,
+        "excludes"          :   ["*.err", "*.out"] # exclude error and output files (relative path from "parent_directory")
+    }
+    # ray.shutdown()
+    ray.init(runtime_env    =   runtime_env)
 
-    # set seed to numpy array of environment randomness
-    env.seed(eval_config["seed"])
-
+    # loop through each episode to evaluate
     for eps in range(eval_config["simulation_loops"]):
         print("\n[INFO]-> Episode Number:\t", eps)
+
+        if eval_config["validation_container"] is not None:
+            sample_row = container_df_sorted.iloc[eps]
+            initial_conditions = {
+                "ego_v1"    :   sample_row["config/ego_v1"],
+                "front_v1"  :   sample_row["config/front_v1"],
+                "front_v2"  :   sample_row["config/front_v2"],
+                "delta_dist":   sample_row["config/delta_dist"]
+            }
+
+            # recall trained model configurations within environment parameters
+            general_config = agent.initialize_config(
+            env_config_path     =   "/env_config.yaml",
+            search_config       =   initial_conditions,
+            model_config_path   =   model_config_name
+            )
+
+            # create environment object with default parameters
+            env = agent.initialize_environment(
+                env_configs     =   general_config["env_config"]
+            )
+
+            # load trained rl model checkpoint
+            model = agent.initialize_model(
+                general_config  =   general_config,
+                log_folder_path =   stats_path
+            )
 
         # run one simulation (episode) loop for defined amount of steps
         statistics = agent.run_episode(
